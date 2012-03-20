60c44aee581dc5378a08e26ac3baa98681dc1c64
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-03-20 14:14:30 +0000

    regex examples

---
+++ Examples/DateTimeExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Examples
+{
+    [TestFixture]
+    public class DateTimeExample
+    {
+        [Test]
+        public void ParseExact()
+        {
+            var date = DateTime.ParseExact("05/10/2011 13:15:15.000", "dd/MM/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture);
+
+            Assert.That(date.Month, Is.EqualTo(10));
+            Assert.That(date.Year, Is.EqualTo(2011));
+            Assert.That(date.Day, Is.EqualTo(05));
+        }
+    }
+}
