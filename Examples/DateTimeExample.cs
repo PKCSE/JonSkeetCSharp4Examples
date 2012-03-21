6e624af6b0b80f6804dceb80d8a1657484de7500
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-03-21 21:55:19 +0000

    datetime and culture

--- Examples/DateTimeExample.cs
+++ Examples/DateTimeExample.cs
@@ -11,7 +11,7 @@ namespace Examples
     public class DateTimeExample
     {
         [Test]
-        public void ParseExact()
+        public void Parse_Exact()
         {
             var date = DateTime.ParseExact("05/10/2011 13:15:15.000", "dd/MM/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture);
 
@@ -19,5 +19,26 @@ namespace Examples
             Assert.That(date.Year, Is.EqualTo(2011));
             Assert.That(date.Day, Is.EqualTo(05));
         }
+
+        [Test]
+        public void TimeSpan_From_Is_More_Readable()
+        {
+            var fiveSeconds1 = TimeSpan.FromSeconds(5);
+            var fiveSeconds2 = new TimeSpan(0, 0, 5);
+
+            Assert.That(fiveSeconds1, Is.EqualTo(fiveSeconds2));
+
+        }
+
+        [Test]
+        public void TimeSpan_Milliseconds_Returns_Only_Milliseconds_Part()
+        {
+            var tenMinute = TimeSpan.FromMinutes(10);
+
+            Assert.That(tenMinute.Milliseconds, Is.EqualTo(0));
+        }
+
+
+
     }
 }
