caf6a6ba268a8d20f018f751492af8897ada37b5
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-03-25 22:08:37 +0100

    Control Flow

---
+++ Examples/ControlFlowExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Examples
+{
+    [TestFixture]
+    public class ControlFlowExample
+    {
+        [Test]
+        public void Return_Is_The_Simplest_Form_Of_Flow_Control()
+        {
+            Console.WriteLine("Hello");
+
+            return;
+            Console.WriteLine("This code is not reachable.");
+        }
+    }
+}
