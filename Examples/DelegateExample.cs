e10ab06181359ffa786f7a0c344a4cac9e138b39
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-04-07 21:17:45 +0100

    delegate

--- Examples/DelegateExample.cs
+++ Examples/DelegateExample.cs
@@ -36,11 +36,95 @@ namespace Examples
             var action3 = action1 + action2;
             action3(20);
         }
+
+        private void ReportToConsole(string text)
+        {
+            Console.WriteLine("Called: {0}", text);
+        }
+
+        [Test]
+        public void Delegate_Can_Invoke_Private_Method()
+        {
+            FakeEventHandler handler = ReportToConsole;
+
+            var raiser = new FakeEventRaiser();
+            raiser.DoSomething("Not subscribed.");
+            
+            raiser.AddHandler(handler);
+            raiser.DoSomething("Subscribed.");
+
+            raiser.AddHandler(handler);
+            raiser.DoSomething("Subscribed twice.");
+
+            raiser.RemoveHandler(handler);
+            raiser.RemoveHandler(handler);
+            raiser.DoSomething("Unbscribed.");
+
+        }
+
+        private void ShortReportToConsole(object sender, EventArgs e)
+        {
+            Console.WriteLine("An event is Called");
+        }
+
+        [Test]
+        public void Event_Makes_It_Simpler()
+        {
+            ClickHandler handler = ShortReportToConsole;
+
+            var raiser = new ShortEventHandler();
+            raiser.OnClick();
+
+            raiser.Click += handler;
+            raiser.OnClick();
+
+            raiser.Click += handler;
+            raiser.OnClick();
+
+            raiser.Click -= handler;
+            raiser.Click -= handler;
+            raiser.OnClick();
+
+        }
+
     }
 
+    public delegate void ClickHandler(object sender, EventArgs e);
+    public class ShortEventHandler
+    {
+        public void OnClick()
+        {
+            ClickHandler clickHandler = Click;
+            if (clickHandler != null)
+                clickHandler.Invoke(this, EventArgs.Empty);
+        }
+
+        public event ClickHandler Click;
+
+    }
+
+    public delegate void FakeEventHandler(string reason);
+    public class FakeEventRaiser
+    {
+        private FakeEventHandler _currentHandler = null;
+        public void AddHandler(FakeEventHandler handler)
+        {
+            _currentHandler = _currentHandler + handler;
+        }
+
+        public void RemoveHandler(FakeEventHandler handler)
+        {
+            _currentHandler = _currentHandler - handler;
+        }
+
+        public void DoSomething(string text)
+        {
+            if (_currentHandler != null)
+                _currentHandler.Invoke(text);
+        }
+    }
 
     public delegate void Int32Action(int value);
-
     public class Target
     {
         private readonly string _name;
